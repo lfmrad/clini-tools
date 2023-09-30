483fd20961c05354e0e0cb3b62f06e0e9d37017d
Luis F. Martínez <lfmrad@gmail.com>
2023-09-30 14:08:32 +0200

    Switched from null handling to Optional

--- src/main/java/xyz/lfmrad/clinitools/util/DataCleaner.java
+++ src/main/java/xyz/lfmrad/clinitools/util/DataCleaner.java
@@ -3,19 +3,17 @@ package xyz.lfmrad.clinitools.util;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import xyz.lfmrad.clinitools.common.AppConstants;
 
 public class DataCleaner {
+
     public static List<Map<String, String>> clean(List<Map<String, String>> data) {
         Iterator<Map<String, String>> iterator = data.iterator();
         while (iterator.hasNext()) {
             Map<String, String> row = iterator.next();
             int currentIndex = data.indexOf(row);
-    
-            Map<String, String> nextRow = null;
-            if (currentIndex < data.size() - 1) {
-                nextRow = data.get(currentIndex + 1);
-            }
+            Optional<Map<String, String>> nextRow = getNextRow(data, currentIndex);
     
             if (isSummaryRow(row) || isSuperfluousData(row, nextRow)) {
                 iterator.remove();
@@ -27,16 +25,24 @@ public class DataCleaner {
         }
         return data;
     }
+
+    private static Optional<Map<String, String>> getNextRow(List<Map<String, String>> data, int currentIndex) {
+        if (currentIndex < data.size() - 1) {
+            return Optional.of(data.get(currentIndex + 1));
+        } else {
+            return Optional.empty();
+        }
+    }
     
     private static boolean isSummaryRow(Map<String, String> row) {
         return row.values().stream().anyMatch(value -> value.contains(AppConstants.SUMMARY_TEXT));
     }
 
-    private static boolean isSuperfluousData(Map<String, String> row, Map<String, String> nextRow) {
+    private static boolean isSuperfluousData(Map<String, String> row, Optional<Map<String, String>> nextRowOpt) {
         if (row.containsValue(AppConstants.DOCUMENT_TEXT)) {
-            if (isRowAssociatedToClient(row) && isNextRowRelated(nextRow)) {
-                // copies name to next row before deleting this one
-                nextRow.put(AppConstants.CLIENT_NAME_KEY, row.get(AppConstants.CLIENT_NAME_KEY));
+            if (isRowAssociatedToClient(row) && nextRowOpt.map(DataCleaner::isNextRowRelated).orElse(false)) {
+                // copies name to next row in preparation before the current one is deleted
+                nextRowOpt.ifPresent(nextRow -> nextRow.put(AppConstants.CLIENT_NAME_KEY, row.get(AppConstants.CLIENT_NAME_KEY)));
             } 
             return true;
         } else {
@@ -49,10 +55,6 @@ public class DataCleaner {
     }
 
     private static boolean isNextRowRelated(Map<String, String> nextRow) {
-        if (nextRow == null) { // prevents error when there is no nextRow available (last item)
-            return false; // 
-        } else {
-            return !nextRow.containsKey(AppConstants.CLIENT_NAME_KEY);
-        }
+        return !nextRow.containsKey(AppConstants.CLIENT_NAME_KEY);
     }
 }
